0e6cbd66a4773c92915a1842ec47e9b1bdafa386
victorferreir <victoremanueljudoferreira@gmail.com>
2022-11-18 16:59:09 -0300

    Tipos_e_variaveis
    Estudo sobre Tipos e Variáveis

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TiposdeVariaveis.java
+        int idadeInt;
+        long idadeLong = 20L;
+        int anoFabricacao = 2020;
+        double salarioMinimoDouble = 2500.50;
+        float salarioMinimoFloat = 2500.50F;
+
+        String meuNome = "Victor Emanuel";//String não é um tipo, é uma classe
+        char letra = 'v';// char -> ' ' / String -> " "
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //constantes
+        int numero = 5; //variável
+        numero = 10;
+        System.out.println(numero);//imprime 10
+
+        final double VALOR_DE_PI = 3.14;//constante
+        //por convenção toda constante deve ser escrita em caixa alta
+
     }
 }
