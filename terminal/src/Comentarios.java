6b8c832a92a48b4c65b81cdc17ff56a10cc24c1d
victorferreir <victoremanueljudoferreira@gmail.com>
2022-11-22 15:36:04 -0300

    javadocs

---
+++ terminal/src/Comentarios.java
@@ -0,0 +1,27 @@
+public class Comentarios {
+    public static void main(String[] args){
+        // Olá, eu sou um comentário em uma única linha
+        
+        /* Olá,
+        * Eu sou um comentario
+        * que posso ser mais detalhado
+        * quando necessário
+        */
+    }
+    /*
+    * Este método foi elaborado as pressas
+    * por isso eu abrevei o nome das variáveis
+    * mas olha, ele tem a finalidade somar ou  multiplicar
+    * dois números
+    */
+    public int somaMultiplica (int n, int x, String m){
+        int r = 0; // r é igual ao resultado
+        if (m == "M"){ // M= multiplicação
+            r= n * x;
+        }else{
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+}
